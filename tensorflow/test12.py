fee1ef408ceef42a17a5ea193e46c000139e59ae
chchae@krict.re.kr <ch1chae@gmail.com>
2022-08-09 07:32:53 +0000

    test

--- tensorflow/test12.py
+++ tensorflow/test12.py
@@ -1,6 +1,73 @@
+from inspect import ArgSpec
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras.datasets.mnist
 
 
+batch_size = 100
+original_dim = 784
+latent_dim = 2
+intermediate_dim = 256
+epochs = 500
+epsilon_std = 1.0
+
+
+
+
+def sampling( args: tuple ) :
+    z_mean, z_log_var = args
+    epsilon = tf.keras.backend.random_normal( shape=(tf.keras.backend.shape(z_mean)[0], latent_dim ), mean=0.0, stddev=epsilon_std )
+    return z_mean + tf.keras.backend.exp( z_log_var / 2 ) * epsilon
+
+
+#def make_encoder() :
+x = tf.keras.layers.Input( shape=(original_dim,), name="input" )
+h = tf.keras.layers.Dense( intermediate_dim, activation='relu', name="encoding" )(x)
+z_mean = tf.keras.layers.Dense( latent_dim, name="mean" )(h)
+z_log_var = tf.keras.layers.Dense( latent_dim, name="log-variance" )(h)
+z = tf.keras.layers.Lambda( sampling, output_shape=(latent_dim,)) ( [ z_mean, z_log_var ] )
+encoder = tf.keras.models.Model( x, [ z_mean, z_log_var, z ], name="encoder" )
+#return encoder
+
+#encoder = make_encoder()
+
+
+
+#def make_decoder() :
+input_decoder = tf.keras.layers.Input( shape=(latent_dim,), name="decoder_input" )
+decoder_h = tf.keras.layers.Dense( intermediate_dim, activation='relu', name="decoder_h" )(input_decoder)
+x_decodeded = tf.keras.layers.Dense( original_dim, activation='sigmoid', name="flat_decoded" )(decoder_h)
+decoder = tf.keras.models.Model( input_decoder, x_decodeded, name="decoder" )
+#return decoder
+
+#decoder = make_decoder()
+
+
+output_combined = decoder( encoder(x)[2] )
+vae = tf.keras.models.Model( x, output_combined )
+vae.summary()
+
+
+
+kl_loss = -0.5 * tf.keras.backend.sum( 1 + z_log_var - tf.keras.backend.exp(z_log_var) - tf.keras.backend.square(z_mean), axis=1 )
+vae.add_loss( tf.keras.backend.mean(kl_loss) / 784.0 )
+vae.compile( optimizer='rmsprop', loss='binary_crossentropy' )
+
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#x_train = tf.convert_to_tensor( x_train, dtype='float', shape=(28*28) )
+#x_test  = tf.convert_to_tensor( x_test, dtype='float' )
+#y_train = tf.one_hot( y_train, depth=10 )
+#y_test  = tf.one_hot( y_test, depth=10 )
+
+x_train = x_train / 255.
+x_test = x_test / 255.
+x_train = x_train.reshape( -1, 28*28 )
+x_test = x_test.reshape( -1, 28*28 )
+
+
+vae.fit( x_train, x_train, shuffle=True, epochs=epochs, batch_size=batch_size )
 
--- tensorflow/test13.py
+++ tensorflow/test13.py
@@ -1,5 +1,11 @@
-from inspect import ArgSpec
-import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import logging
+logging.getLogger('tensorflow').disabled = True
+from silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+
+from tensorflow import keras as K
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,29 +21,110 @@ z_dim = 100
 
 
 def build_generator( img_shape, z_dim ) :
-    model = tensorflow.keras.models.Sequential()
-    model.add( tensorflow.keras.layers.Dense( 128, input_dim=z_dim ) )
-    model.add( tensorflow.keras.layers.LeakyReLU( alpha=0.01 ) )
-    model.add( tensorflow.keras.layers.Dense( 28 * 28 * 1, activation='tanh' ) )
-    model.add( tensorflow.keras.layers.Reshape( img_shape ) )
+    model = K.models.Sequential()
+    model.add( K.layers.Dense( 128, input_dim=z_dim ) )
+    model.add( K.layers.LeakyReLU( alpha=0.01 ) )
+    model.add( K.layers.Dense( 28 * 28 * 1, activation='tanh' ) )
+    model.add( K.layers.Reshape( img_shape ) )
     return model
 
 generator = build_generator( img_shape, z_dim )
+generator.compile( loss='binary_crossentropy', optimizer=K.optimizers.Adam() )
 
 
 
 def build_descriminator( img_shape, z_dim ) :
-    model = tensorflow.keras.models.Sequential()
-    model.add( tensorflow.keras.layers.Flatten( input_shape=img_shape ) )
-    model.add( tensorflow.keras.layers.Dense( 128 ) )
-    model.add( tensorflow.keras.layers.LeakyReLU( alpha=0.01 ) )
-    model.add( tensorflow.keras.layers.Dense( 1, activation='sigmoid' ) )
+    model = K.models.Sequential()
+    model.add( K.layers.Flatten( input_shape=img_shape ) )
+    model.add( K.layers.Dense( 128 ) )
+    model.add( K.layers.LeakyReLU( alpha=0.01 ) )
+    model.add( K.layers.Dense( 1, activation='sigmoid' ) )
     return model
 
 discriminator = build_descriminator( img_shape, z_dim )
+discriminator.compile( loss='binary_crossentropy', optimizer=K.optimizers.Adam(), metrics=['accuracy'] )
+discriminator.trainable = False
 
 
+def build_gan( generator, discriminator ) :
+    model = K.Sequential()
+    model.add( generator )
+    model.add( discriminator )
+    return model
 
+gan = build_gan( generator, discriminator )
+gan.compile( loss='binary_crossentropy', optimizer=K.optimizers.Adam() )
+
+
+if False  :
+    batch_size = 128
+    z = np.random.normal( 0, 1, (batch_size, 100) )
+    gen_imgs = generator.predict(z)
+    print( gen_imgs.shape )    # (128, 28, 28, 1)
+    fig, axs = plt.subplots( 1, 4, figsize=(4,4), sharey=True, sharex=True )
+    print( axs.shape )
+    axs = axs.reshape( 1, 4 )
+    img = gen_imgs[ 0, :, :, 0 ]
+    axs[0,0].imshow( img, cmap='gray' )
+    exit(0)
+
+
+losses = []
+accuracies = []
+iteraction_checkpoints = []
+
+def train( iterations, batch_size, sample_interval ) :
+    (X_train, _), (_,_) = K.datasets.mnist.load_data()
+    X_train = X_train / 127.5 - 1.0
+    print( X_train.shape )
+    X_train = np.expand_dims( X_train, axis=3 )
+    print( X_train.shape )
+    real = np.ones( (batch_size, 1) )
+    fake = np.zeros( (batch_size, 1) )
+
+    for iteration in range(iterations) :
+        idx = np.random.randint( 0, X_train.shape[0], batch_size )
+        imgs = X_train[idx]
+        z = np.random.normal( 0, 1, (batch_size, 100) )
+        gen_imgs = generator.predict(z,verbose=3)
+
+        d_loss_real = discriminator.train_on_batch( imgs, real )
+        d_loss_fake = discriminator.train_on_batch( gen_imgs, fake )
+        d_loss, accuracy = 0.5 * np.add( d_loss_real, d_loss_fake )
+        accuracy = 0
+
+        z = np.random.normal( 0, 1, (batch_size, 100) )
+        gen_imgs = generator.predict(z,verbose=3)
+        g_loss = gan.train_on_batch( z, real, )
+
+        if( iteration + 1 ) % sample_interval == 0 :
+            losses.append( (d_loss, g_loss) )
+            accuracies.append( 100.0 * accuracy )
+            iteraction_checkpoints.append( iteration + 1 )
+
+            print( "%d [D loss: %f, accuracy : %.2f%%] [G loss: %f]" % (iteration + 1, d_loss, 100.0*accuracy, g_loss) )
+            sample_images( generator )
+
+
+def sample_images( generator, image_grid_rows=1, image_grid_cols=4 ) :
+    z = np.random.normal( 0, 1, (image_grid_rows * image_grid_cols, z_dim) )
+    gen_imgs = generator.predict(z,verbose=3)
+    fig, axs = plt.subplots( image_grid_rows, image_grid_cols, figsize=(4,4), sharey=True, sharex=True )
+    axs = np.expand_dims( axs, axis=0 )
+
+    cnt = 0
+    for i in range( image_grid_rows ) :
+        for j in range( image_grid_cols ) :
+            axs[i,j].imshow( gen_imgs[ cnt, :, :, 0 ], cmap='gray' )
+            axs[i,j].axis('off')
+            plt.imsave( 'test13.png', gen_imgs[ cnt, :, :, 0 ], cmap='gray' )
+            cnt += 1
+
+
+iterations = 60000
+batch_size = 128
+sample_interval = 1000
+train( iterations, batch_size, sample_interval )
 
 
 
